f6419007cd57d273bc6e14dc3ad915b4305a9875
CF8339816 <chris_french_3dga@hotmail.com>
2026-01-15 22:42:37 -0400

    added damage and level up

--- prog2_challAlpha_playerClassUpgrade_ChrisFrench0259182/Program.cs
+++ prog2_challAlpha_playerClassUpgrade_ChrisFrench0259182/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Policy;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace prog2_challAlpha_playerClassUpgrade_ChrisFrench0259182
@@ -10,6 +13,9 @@ namespace prog2_challAlpha_playerClassUpgrade_ChrisFrench0259182
     {
         static void Main(string[] args)
         {
+
+           
+
             Player player1 = new Player();
             Player player2 = new Player();
             player1.SetName("Generic Hero 1");
@@ -18,6 +24,20 @@ namespace prog2_challAlpha_playerClassUpgrade_ChrisFrench0259182
             Console.WriteLine(" player stats ");
             player1.HUD();
             player2.HUD();
+
+
+            Console.ReadKey(true);
+
+            player1.TakeDamage();
+
+            player2.TakeDamage();
+
+
+
+
+
+
+
 
 
 
@@ -64,6 +84,27 @@ namespace prog2_challAlpha_playerClassUpgrade_ChrisFrench0259182
             Console.WriteLine($"{_level}");
         }
 
+
+        public void TakeDamage()
+        {
+
+            Random Dmg = new Random();
+            int amount = Dmg.Next(5, 21);
+
+            _currentHealth -= amount;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"\n {_name} took ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"{amount} ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"damage  ");
+            if (_currentHealth < 0) _currentHealth = 0;
+        }
+
+        public void LevelUp()
+        {
+            _level++;
+        }
 
 
 
